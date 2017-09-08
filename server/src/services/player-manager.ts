ae6ee699dcf2620b0ed6ab9ebdca9403114015f3
Nigel Anderton <nigel.anderton@hitched.email>
2017-09-08 16:59:10 +0100

    Queue Play Working

--- server/src/services/player-manager.ts
+++ server/src/services/player-manager.ts
@@ -1,12 +1,15 @@
 import * as logger from 'morgan';
 import * as socketIo from "socket.io";
-import * as amqp from 'amqplib/callback_api'
+import * as amqp from 'amqplib/callback_api';
+import * as fetch from 'node-fetch';
 
 import { Observable } from 'rxjs/Observable';
 import { ISubscription } from "rxjs/Subscription";
 
 import { RabbitMQService } from './rabbit-mq';
 import { QueueManagerService } from './queue-manager';
+import { QueueManagerRequest } from '../models/shared/queue-manager/queue-manager-request';
+import { QueueManagerTrackRequest } from '../models/shared/queue-manager/queue-manager-track-request';
 
 import { NowPlayingService } from './now-playing';
 import { NowPlayingItem } from '../models/shared/now-playing/now-playing-item';
@@ -21,6 +24,9 @@ export class PlayerManagerService {
     private rabbit: RabbitMQService;
     private queueManager: QueueManagerService
     private playerConnection: ISubscription;
+
+    private AccessTokem: string;
+    private RefreshToken: string;
 
     public static bootstrap(
         rabbit: RabbitMQService, 
@@ -58,37 +64,69 @@ export class PlayerManagerService {
     }
 
     private listen() {
-        this.playerConnection = this.rabbit.getMessagesObervable(RabbitMQService.RS_PLAYER_Q).subscribe((result): any => {
+        this.playerConnection = this.rabbit.getMessagesObervable(RabbitMQService.RS_PLAYER_Q).subscribe((qmRequest: any): void => {
+
+            qmRequest = QueueManagerRequest.FromObject(JSON.parse(qmRequest));
+            let trackRequest = QueueManagerTrackRequest.FromObject(qmRequest.GetData() as QueueManagerTrackRequest);
+
+            this.AccessTokem = trackRequest.GetAccessToken();
+            this.RefreshToken = trackRequest.GetAccessToken();
+
+            console.log("Track Request Recieved.");
+            console.log("Access Token : " + this.AccessTokem);
+            console.log("Refresh Token : " + this.RefreshToken);
 
             if(this.queueManager.FetchItemCount() > 0 ) {
-                this.rabbit.sendMessage(
-                    RabbitMQService.RS_PLAYLIST_Q, 
-                    this.queueManager.FetchItem()
-                );
-                let nowPlayingResponse: NowPlayingResponse = new NowPlayingResponse(
-                    this.queueManager.FetchQueue()
-                );
-                this.io.emit(
-                    NowPlayingResponse.fetchNowPlayingResponseHook(this.appPrefix, NowPlayingService.SERVICE_PREFIX),
-                    nowPlayingResponse
-                );
-            } else {
-                this.rabbit.sendMessage(
-                    RabbitMQService.RS_PLAYLIST_Q, 
-                    new NowPlayingItem(-1, "", "", "", "", "")
-                );
+                console.log("Sending Play Command!");
+                let nowPlayingItem = this.queueManager.FetchItem();
+                let uri: string = "spotify:track:" + nowPlayingItem.getId();
+                console.log("Uri:");
+                console.log(uri);
+                this.sendGeneric('https://api.spotify.com/v1/me/player/play', {
+                    "uris": [
+                        uri
+                    ]
+                }).then(response => {
+                    console.log(response);
+                    let nowPlayingResponse: NowPlayingResponse = new NowPlayingResponse(
+                        this.queueManager.FetchQueue()
+                    );
+                    this.io.emit(
+                        NowPlayingResponse.fetchNowPlayingResponseHook(this.appPrefix, NowPlayingService.SERVICE_PREFIX),
+                        nowPlayingResponse
+                    );
+                });
             }
+
+            // if(this.queueManager.FetchItemCount() > 0 ) {
+            //     this.rabbit.sendMessage(
+            //         RabbitMQService.RS_PLAYLIST_Q, 
+            //         this.queueManager.FetchItem()
+            //     );
+            //     let nowPlayingResponse: NowPlayingResponse = new NowPlayingResponse(
+            //         this.queueManager.FetchQueue()
+            //     );
+            //     this.io.emit(
+            //         NowPlayingResponse.fetchNowPlayingResponseHook(this.appPrefix, NowPlayingService.SERVICE_PREFIX),
+            //         nowPlayingResponse
+            //     );
+            // } else {
+            //     this.rabbit.sendMessage(
+            //         RabbitMQService.RS_PLAYLIST_Q, 
+            //         new NowPlayingItem(-1, "", "", "", "", "")
+            //     );
+            // }
             
         });
     }
 
-    // private sendGeneric(url: string, body: any, method: string = 'put'): Promise<any> {
-    //     return fetch(url, {
-    //         method: method,
-    //         headers: { Authorization: 'Bearer ' + this.accessToken },
-    //         body: JSON.stringify(body)
-    //     });
-    // }
+    private sendGeneric(url: string, body: any, method: string = 'put'): Promise<any> {
+        return fetch(url, {
+            method: method,
+            headers: { Authorization: 'Bearer ' + this.AccessTokem },
+            body: JSON.stringify(body)
+        });
+    }
 
     private logQueue(): void {
 
